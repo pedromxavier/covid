2f8eb4c7e46a1cda982012624573118856706c39
Pedro <pedromxavier@poli.ufrj.br>
2020-05-05 06:32:50 -0300

    rearrange path

---
+++ api/__init__.py
@@ -0,0 +1,2 @@
+from .main import API
+from .plotter import Plotter
--- api/plotter.py
+++ api/plotter.py
@@ -26,57 +26,50 @@ class Plotter:
     }
 
     CAUSE_YEAR = [f'{cause}_{year}' for cause, year in API.CAUSE_YEAR]
-    
-    x = []
-    y = {}
 
-    INIT = False
+    def __init__(self, fname:str):
+        self.fname = fname
+        self.x, self.y = self.parse_csv(self.fname) 
 
-    @classmethod
-    def year_diff(cls, cause):
+    def year_diff(self, cause):
         assert cause != 'COVID'
-        return cls.y[f'{cause}_2020'] - cls.y[f'{cause}_2019']
+        return self.y[f'{cause}_2020'] - self.y[f'{cause}_2019']
 
-    @classmethod
-    def diff_covid_resp(cls, **kwargs):
+    def diff_covid_resp(self, **kwargs):
         fig, ax = plt.subplots()
 
-        ax.plot(cls.x, cls.y['COVID_2020'], label='COVID 2020')
-        ax.plot(cls.x, sum(cls.year_diff(cause) for cause in cls.RESPIRATORY), label='DOENÇAS RESPITÓRIAS 2020 - 2019')
+        ax.plot(self.x, self.y['COVID_2020'], label='COVID 2020')
+        ax.plot(self.x, sum(self.year_diff(cause) for cause in self.RESPIRATORY), label='DOENÇAS RESPITÓRIAS 2020 - 2019')
 
         plt.legend()
+        plt.title('Aumento das doenças respitatórias vs. COVID')
 
-        cls.plot(**kwargs)
+        self.plot(**kwargs)
 
-    @classmethod
-    def plot_all(cls, **kwargs):
+    def plot_all(self, **kwargs):
         fig, ax = plt.subplots()
-        for cause_year in cls.CAUSE_YEAR:
-            ax.plot(cls.x, cls.y[cause_year], label=cause_year)
+        for cause_year in self.CAUSE_YEAR:
+            ax.plot(self.x, self.y[cause_year], label=cause_year)
         plt.legend()
-        cls.plot(**kwargs)
+        self.plot(**kwargs)
 
-    @classmethod
-    def compare_cities(cls, *cities, **kwargs):
+    def compare_cities(self, *cities, **kwargs):
         ...
 
-    @classmethod
-    def compare_causes(cls, *causes, **kwargs):
+    def compare_causes(self, *causes, **kwargs):
         ...
 
-    @classmethod
-    def compare_years(cls, *causes):
+    def compare_years(self, *causes):
         n = len(causes)
-        ncols = min(cls, cls.MAX_COLS, n)
+        ncols = min(self, self.MAX_COLS, n)
         nrows = (n//ncols)
         fig, axs = plt.subplots((n//ncols), ncols)
         for i in range(nrows):
             for j in range(ncols):
                 cause = causes[i*nrows + j]
-                axs[i, j].plot(cls.x, np.cumsum(cls.y[cause]))
+                axs[i, j].plot(self.x, np.cumsum(self.y[cause]))
 
-    @classmethod
-    def plot(cls, fname: str=None, **kwargs):
+    def plot(self, fname: str=None, **kwargs):
 
         ## Adiciona Título ao plot.
         if 'title' in kwargs: plt.title(kwargs['title'])
@@ -91,7 +84,7 @@ class Plotter:
             plt.savefig(fname)
 
     @classmethod
-    def init_csv(cls, fname: str):
+    def parse_csv(cls, fname: str) -> (list, dict):
         if not fname.endswith('.csv'):
             fname = f'{fname}.csv'
 
@@ -118,10 +111,8 @@ class Plotter:
                     y[cause].pop(-1)
                 y[cause] = np.cumsum(y[cause], dtype=np.float64)
                 y[cause][y[cause] == 0] = np.nan
-        cls.x = x
-        cls.y = y
-        cls.INIT = True
+        return x, y
 
 if __name__ == '__main__':
-    Plotter.init_csv('RJ')
-    Plotter.diff_covid_resp(title='Estado do Rio de Janeiro')
+    plot = Plotter('RJ')
+    plot.diff_covid_resp(title='Estado do Rio de Janeiro')
