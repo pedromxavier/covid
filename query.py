8caef81e67f56236a65e443e64a9cea30ffc718b
Pedro Maciel Xavier <pedromxavier@poli.ufrj.br>
2020-05-24 16:57:22 -0300

    Update Query

--- query.py
+++ query.py
@@ -5,25 +5,31 @@ x = api.API()
 
 x.login()
 
-query = api.APIQuery(
-    start_date=datetime.date(2019, 1, 1),
-    end_date=datetime.date(2020, 5, 23),
-    state='RJ',
-    city_id='4646',
-    chart='chart5',
-    places=list(x.PLACES)
-    )
+chart=''
 
-results = api.APIResults(
-    date=datetime.date(2020, 5, 23),
-    state='RJ',
-    city='Rio de Janeiro',
-    place="&".join(x.PLACES)
-    )
+requests = []
 
-request = api.APIRequest(x.API_URL, query, results, headers=x.REQUEST_HEADERS)
-print(request.request.full_url)
-ans = x.make_request(request)
+for place in x.PLACES:
 
-with open(f'results-TODOS.json', 'w') as file:
-    file.write(ans.read().decode())
+    query = api.APIQuery(
+        start_date=datetime.date(2019, 1, 1),
+        end_date=datetime.date(2020, 5, 23),
+        state='RJ',
+        city_id=x.city_id('RJ', 'Rio de Janeiro'),
+        chart=chart
+        places=[place]
+        )
+
+    results = api.APIResults(
+        date=datetime.date(2020, 5, 23),
+        state='RJ',
+        city='Rio de Janeiro',
+        place=place
+        )
+
+    request = api.APIRequest(x.API_URL, query, results, headers=x.REQUEST_HEADERS)
+    print(request.request.full_url)
+    ans = x.make_request(request)
+
+    with open(f'results-{place}-teste.json', 'w') as file:
+        file.write(ans.read().decode())
