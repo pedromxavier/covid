c6ba13a5e03509748ae633415589da9aabfe4fd1
Pedro <pedromxavier@poli.ufrj.br>
2020-06-01 17:45:10 -0300

    Add Query

--- query-rj.py
+++ query-rj.py
@@ -6,7 +6,7 @@ def main(date=all, city=all, state=all, gender=all, age=True, places=all, thread
     ## Define os parâmetros da busca
     api.API(
         date=date, ## busca para todas as datas de 2019-01-01 até hoje
-        city='Rio de Janeiro', ## busca para todas as cidades no estados especificados abaixo
+        city=city, ## busca para todas as cidades no estados especificados abaixo
         state='RJ', ## busca para todos os estados
         gender=gender, ## busca para cada sexo
         age=age,  ## busca por faixa etária
@@ -15,8 +15,4 @@ def main(date=all, city=all, state=all, gender=all, age=True, places=all, thread
     ).get() ## coleta os dados e salva em csv
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='API de busca para dados dos cartórios.')
-    parser.add_argument('--gender', type=str, dest='gender', help='gender options', default=all, choices=['M', 'F'])
-    parser.add_argument('--threads', type=int, dest='threads', help='number of threads', default=os.cpu_count())
-    args = parser.parse_args()
-    main(gender=args.gender, threads=args.threads)
+    main()
